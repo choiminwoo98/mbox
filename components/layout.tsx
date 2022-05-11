95583e937c199147258ae5046e451ac55a126ef1
choiminwoo98 <61531483+choiminwoo98@users.noreply.github.com>
2022-05-11 13:17:48 +0900

    modal, NavBar,
    앨범 생성,삭제,수정에 관한 모달(기능X)

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,20 +2,20 @@ import useUser, { UserWithAccount } from "@libs/client/useUser";
 import type { NextPage } from "next";
 
 export interface BaseProps {
-    user: UserWithAccount | undefined;
+  user: UserWithAccount | undefined;
 }
 
 interface LayoutProps {
-    FC: React.FC<BaseProps>;
+  FC: React.FC<BaseProps>;
 }
 
 const Layout: NextPage<LayoutProps> = ({ FC }) => {
-    const { user } = useUser();
-    return (
-        <div className="flex">
-            <FC user={user} />
-        </div>
-    );
+  const { user } = useUser();
+  return (
+    <div className="flex">
+      <FC user={user} />
+    </div>
+  );
 };
 
 export default Layout;
